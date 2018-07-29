647460361ea7f752b32e1a7580826db88ca2f853
anshnagrath <anshnagrath448@gmail.com>
2018-07-29 08:46:18 +0530

    almost there

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,18 +1,30 @@
+
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Recipe } from '../../models/reciepe';
+
 @IonicPage()
 @Component({
   selector: 'page-recipe',
   templateUrl: 'recipe.html',
 })
-export class RecipePage {
+export class RecipePage implements OnInit {
+  recipe: Recipe;
+  index: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.recipe = this.navParams.get('recipe');
+    console.log(this.recipe, 'look')
   }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, { mode: 'New' })
 
+  }
+  ngOnInit() {
+    this.recipe = this.navParams.get('recipe');
+
+    this.index = this.navParams.get('index');
   }
 }
--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,10 @@
+import { RecipesService } from './../../services/reciepe';
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RecipePage } from '../recipe/recipe';
+import { Recipe } from '../../models/reciepe';
+
 
 
 
@@ -10,14 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+  public recepies;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public resSer: RecipesService) {
   }
-
+  ionViewWillEnter() {
+    this.recepies = this.resSer.getRecipies();
+    console.log(this.recepies, 'check this one ')
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecipesPage');
   }
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, { mode: 'new' })
+  }
+  onLoadRecepie(recipe: Recipe, index: number) {
+    console.log(recipe, 'have to check it here')
+    this.navCtrl.push(RecipePage, { recipe: recipe, index: index });
   }
 }
